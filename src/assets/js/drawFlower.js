8c36d0b44f140761e5d88762847a2447d8cf5683
oaugusto <oaugusto@outlook.com>
2017-05-24 16:31:25 -0300

    update

--- src/assets/js/drawFlower.js
+++ src/assets/js/drawFlower.js
@@ -3,12 +3,10 @@ var draw = function() {
     
     var rFlowerBud = 1;
     var ryPetal = 5;
-    var xScale;
-    var yScale;
 
-    var elements = g.selectAll("g.vis-elements").data(["vis-elements"]);
+    var elements = g.selectAll("g.infoPaises").data(["infoPaises"]);
     elements.exit().remove();
-    elements = elements.enter().append('g').attr('class', "vis-elements").merge(elements);
+    elements = elements.enter().append('g').attr('class', "infoPaises").merge(elements);
 
     var flowers = elements.selectAll("g.flower").data(data); //UPDATE
     flowers.exit().remove(); //EXIT
@@ -16,7 +14,7 @@ var draw = function() {
     
     flowers.attr('transform', function(d) {
         //xScale and yScale must be change to flowerVisualization.x and y
-        return "translate("+ (xScale(d.country)) + (xScale.bandwidth()/2)+ "," + (yScale(+d.mean))+ ")";
+        return "translate("+ + "," + + ")";
     }).each(function (e) {
         
         //center
@@ -29,20 +27,6 @@ var draw = function() {
         //TODO change this metrics
         var metrics = Object.keys(e).filter(function (k) { return k !== "country";});
 
-        /*
-        //Petals line
-        var flowerPetal = d3.select(this).selectAll(".flower-petal").data(metrics); //UPDATE
-        flowerPetal.exit().remove(); //EXIT
-        flowerPetal = flowerPetal.enter().append('line').attr('class', 'flower-petal').merge(flowerPetal); //ENTER
-        flowerPetal.attr()
-        .attr('x1', 0)
-        .attr('y1', 0)
-        .attr('x2', function(k, i) {return petalScale(k)+e[k] * Math.cos(((2 * Math.PI)/metrics.length) * i)})
-        .attr('y2', function(k, i) {return petalScale(k)+e[k] * Math.sin(((2 * Math.PI)/metrics.length) * i)})
-        .style('fill', 'none')
-        .style('stroke', function (k){ return colorScale(k); });
-        */
-        
         //Petals ellipse 
         var flowerPetal = d3.select(this).selectAll(".flower-petal").data(metrics); //UPDATE
         flowerPetal.exit().remove(); //EXIT
@@ -53,13 +37,13 @@ var draw = function() {
         .attr('cy', 0)
         .attr('rx', petalScale(k)+e[k]) //length of ellipse TODO
         .attr('ry', 5) //width fix 
-        .style('fill',function (k){ return colorScale(k); })
+        .style('fill',function (k){ return colorScale(k); }) //do colorScale
         .attr( 'transform', function (k, i) {
             return d3.svg.transform()
             .rotate( (360/metrics.length) * i)
             .translate((rFlowerBud + (d3.select(this).rx / 2)) * Math.cos(((2 * Math.PI)/metrics.length) * i)
             ,(rFlowerBud + (d3.select(this).rx / 2)) * Math.sin(((2 * Math.PI)/metrics.length) * i));
-        })
+        });
 
         //label
         var Label = d3.select(this).selectAll(".flower-label").data([e]); //UPDATE
